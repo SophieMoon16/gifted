4ff23c286c58a348848f50bfd2ce95221904fb7f
SophieMoon16 <sluu160593@gmail.com>
2026-02-20 16:07:51 +0100

    db ssl fix

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,10 +1,7 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: { rejectUnauthorized: true }, // mode sécurisé
 });
