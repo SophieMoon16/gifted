382cb663da743775bcb573b0f78c03f278eaeeaa
SophieMoon16 <sluu160593@gmail.com>
2026-02-20 16:13:12 +0100

    db ssl

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -3,5 +3,5 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: { rejectUnauthorized: true }, // mode sécurisé
+  ssl: { rejectUnauthorized: false }, // mode sécurisé
 });
